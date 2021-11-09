bfb1d58e35864ed3f70c5148524f0586dd1b024b
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-11-09 20:55:01 +0000

    Update pxt.json, main.ts, ps2.ts

--- main.ts
+++ main.ts
@@ -1,104 +1,31 @@
+ enum ButtonText {
+    "START",
+    "SELECT",
+    "PAD_UP",
+    "PAD_DOWN",
+    "PAD_LEFT",
+    "PAD_RIGHT",
+    "TRIANGLE",
+    "CROSS",
+    "SQUARE",
+    "CIRCLE",
+    "L1",
+    "R1",
+    "L2",
+    "R2",
+    "L3",
+    "R3"
+}
 
-namespace TargetArchitecture.PS2 {
+let X = ButtonText.PAD_DOWN
 
-    let topics: string[] = ["ps2/sticks/left", "ps2/sticks/right", "ps2/buttons", "ps2/rumble", "ps2/info", "ps2/ip", "ps2/dial", "ps2/error"];
+basic.showString(X.toString())
 
-    export declare const enum Buttons {
-        //% block="START"
-        START = 0,
-        //% block="SELECT"
-        SELECT = 1,
-        //% block="PAD_UP"
-        PAD_UP = 2,
-        //% block="PAD_DOWN"
-        PAD_DOWN = 3,
-        //% block="PAD_LEFT"
-        PAD_LEFT = 4,
-        //% block="PAD_RIGHT"
-        PAD_RIGHT = 5,
-        //% block="TRIANGLE"
-        TRIANGLE = 6,
-        //% block="CROSS"
-        CROSS = 7,
-        //% block="SQUARE"
-        SQUARE = 8,
-        //% block="CIRCLE"
-        CIRCLE = 9,
-        //% block="L1"
-        L1 = 10,
-        //% block="R1"
-        R1 = 11,
-        //% block="L2"
-        L2 = 12,
-        //% block="R2"
-        R2 = 13,
-        //% block="L3"
-        L3 = 14,
-        //% block="R3"
-        R3 = 15
-    }
+//basic.showString(ButtonText["L2"].toString());
 
-    let pinOffset = 1000;
+let keys = Object.keys(X);
 
-    export function connectController() {
-        for (let topic of topics) {
-            RainbowSparkleUnicorn.IoT.listen(topic);
-        }
-    }
+for (let button of keys) {
 
-    export const PS2_CONTROLLER_BUTTON_PRESSED_ANY = 6009;
-    export const PS2_CONTROLLER_BUTTON_PRESSED = 6010;
-    //... 16 Buttons to 6026 
-
-    export function processNewMQTTMessage(message: string) {
-        if (message.includes("PAD_LEFT")) {
-            control.raiseEvent(PS2_CONTROLLER_BUTTON_PRESSED + pin, pin + pinOffset)
-            control.raiseEvent(PS2_CONTROLLER_BUTTON_PRESSED_ANY, pin + pinOffset)
-
-        }
-    }
-
-
-
-    /**
- * Do something when a switch is pushed.
- * @param btn the controller button to be checked
- * @param handler body code to run when the button is pressed
- */
-    //% subcategory="Button"
-    //% block="on button %btn"
-    //% weight=65
-    export function onPressed(
-        btn: Buttons,
-        handler: () => void
-    ) {
-        control.onEvent(
-            PS2_CONTROLLER_BUTTON_PRESSED + btn,
-            EventBusValue.MICROBIT_EVT_ANY,
-            () => {
-                handler();
-            }
-        );
-
-    }
-
-    /**
-* Do something when any button is pressed.
-* @param handler body code to run when any button is pressed
-*/
-    //% subcategory="Button"
-    //% block="on any any button"
-    //% weight=65
-    export function onAnyPressed(
-        handler: (btn: Buttons) => void
-    ) {
-        control.onEvent(
-            PS2_CONTROLLER_BUTTON_PRESSED_ANY,
-            EventBusValue.MICROBIT_EVT_ANY,
-            () => {
-                handler(control.eventValue() - pinOffset);
-            }
-        );
-    }
-
+    basic.showString(button)
 }
---
+++ ps2.ts
@@ -0,0 +1,123 @@
+namespace TargetArchitecture.PS2 {
+
+    let topics: string[] = ["ps2/sticks/left", "ps2/sticks/right", "ps2/buttons", "ps2/rumble"];
+    let btns: string[] = ["START", "SELECT", "PAD_UP", "PAD_DOWN", "PAD_LEFT", "PAD_RIGHT", "TRIANGLE", "CROSS", "SQUARE", "CIRCLE", "L1", "R1", "L2", "R2", "L3", "R3"];
+
+    export const enum Buttons {
+        //% block="START"
+        START = 0,
+        //% block="SELECT"
+        SELECT = 1,
+        //% block="PAD_UP"
+        PAD_UP = 2,
+        //% block="PAD_DOWN"
+        PAD_DOWN = 3,
+        //% block="PAD_LEFT"
+        PAD_LEFT = 4,
+        //% block="PAD_RIGHT"
+        PAD_RIGHT = 5,
+        //% block="TRIANGLE"
+        TRIANGLE = 6,
+        //% block="CROSS"
+        CROSS = 7,
+        //% block="SQUARE"
+        SQUARE = 8,
+        //% block="CIRCLE"
+        CIRCLE = 9,
+        //% block="L1"
+        L1 = 10,
+        //% block="R1"
+        R1 = 11,
+        //% block="L2"
+        L2 = 12,
+        //% block="R2"
+        R2 = 13,
+        //% block="L3"
+        L3 = 14,
+        //% block="R3"
+        R3 = 15
+    }
+
+    let pinOffset = 1000;
+
+    /**
+     * Connect the PS2 controller
+    */
+    //% subcategory="PS2"
+    //% weight=30
+    export function connectController() {
+        for (let topic of topics) {
+            RainbowSparkleUnicorn.IoT.listen(topic);
+        }
+    }
+
+    const PS2_CONTROLLER_BUTTON_PRESSED_ANY = 6009;
+    const PS2_CONTROLLER_BUTTON_PRESSED = 6010;
+    //... 16 Buttons to 6026 
+
+
+    /**
+    * Process the PS2 MQTT messages
+    * @param topic of the MQTT message
+    * @param message the MQTT message
+    */
+    //% subcategory="PS2"
+    //% block="process MQTT message for topic %topic with content %message"
+    //% weight=65
+    export function processNewMQTTMessage(topic: string, message: string) {
+
+        if (topic.includes("ps2/buttons")) {
+            //button topic
+            for (let pin = 0; pin < btns.length; pin++) {
+                let btn = btns[pin];
+                if (message.includes(btn)) {
+                    control.raiseEvent(PS2_CONTROLLER_BUTTON_PRESSED + pin, pin + pinOffset)
+                    control.raiseEvent(PS2_CONTROLLER_BUTTON_PRESSED_ANY, pin + pinOffset)
+                }
+            }
+        }
+    }
+
+
+    /**
+ * Do something when a switch is pushed.
+ * @param btn the controller button to be checked
+ * @param handler body code to run when the button is pressed
+ */
+    //% subcategory="PS2"
+    //% block="on button %btn"
+    //% weight=65
+    export function onPressed(
+        btn: Buttons,
+        handler: () => void
+    ) {
+        control.onEvent(
+            PS2_CONTROLLER_BUTTON_PRESSED + btn,
+            EventBusValue.MICROBIT_EVT_ANY,
+            () => {
+                handler();
+            }
+        );
+
+    }
+
+    /**
+* Do something when any button is pressed.
+* @param handler body code to run when any button is pressed
+*/
+    //% subcategory="PS2"
+    //% block="on any any button"
+    //% weight=65
+    export function onAnyPressed(
+        handler: (btn: Buttons) => void
+    ) {
+        control.onEvent(
+            PS2_CONTROLLER_BUTTON_PRESSED_ANY,
+            EventBusValue.MICROBIT_EVT_ANY,
+            () => {
+                handler(control.eventValue() - pinOffset);
+            }
+        );
+    }
+
+}
